7cfbdabe83ca2d7ca3794b6819f8b74d472bbd20
Sarah <sarahhawkins26@gmail.com>
2023-06-06 18:00:30 -0700

    initial

---
+++ 02-Challenge/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/develop/public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/develop/public/index.html'));
+});
