7458d3da2b694b7a0f385411cbe8a3b1ed8e9af1
Sarah <sarahhawkins26@gmail.com>
2023-06-09 16:25:18 -0700

    working

--- 02-Challenge/server.js
+++ 02-Challenge/server.js
@@ -12,13 +12,9 @@ app.use(express.static('public'));
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
-//default route to index.html
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
-});
 
 app.get('/api/notes', (req, res) => {
-    fs.readFile(path.join(__dirname, '../db/db.json'), (err, data) => {
+    fs.readFile(path.join(__dirname, '/db/db.json'), (err, data) => {
         if (err) {
             console.log(err);
             return res.status(500).json({ error: 'Failed to read notes.'});
@@ -30,27 +26,32 @@ app.get('/api/notes', (req, res) => {
 
 app.post('/api/notes', (req, res) => {
     const newNote = req.body;
-    fs.readFile(path.join(__dirname, '../db/db.json'), (err, data) => {
+    fs.readFile(path.join(__dirname, '/db/db.json'), (err, data) => {
         if (err) {
             console.log(err);
             return res.status(500).json({ error: 'Failed to save notes.'});
         }
         const notes = JSON.parse(data)
-       if (notes.length > 0) {
-        newNote.id = notes[notes.length -1].id +1;
-       } else {
-        newNote.id = 1;
-       }
-       notes.push(newNote);
-
-       fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(notes), (err) => {
-        if (err) {
-            console.log(err);
-            return res.status(500).json({ error: 'Failed to save notes.'});
+        if (notes.length > 0) {
+            newNote.id = notes[notes.length -1].id +1;
+        } else {
+            newNote.id = 1;
         }
-        res.json(newNote);
-       });
+        notes.push(newNote);
+        
+        fs.writeFile(path.join(__dirname, '/db/db.json'), JSON.stringify(notes), (err) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).json({ error: 'Failed to save notes.'});
+            }
+            res.json(newNote);
+        });
     });
+});
+
+//default route to index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.listen(PORT, () => {
